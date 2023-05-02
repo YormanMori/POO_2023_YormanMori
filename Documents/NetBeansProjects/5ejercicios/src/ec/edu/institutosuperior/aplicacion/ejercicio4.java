4c14c4a18d93b2cecf2cdfc74e26296668209844
User <User@YORMAN>
2023-05-02 15:12:51 -0500

    Numeros pares

---
+++ Documents/NetBeansProjects/5ejercicios/src/ec/edu/institutosuperior/aplicacion/ejercicio4.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.institutosuperior.aplicacion;
+
+/**
+ *
+ * @author User
+ */
+public class ejercicio4 {
+     public static void main(String[] args) {
+        for (int numeropar = 2; numeropar <= 100; numeropar += 2) {
+            System.out.println(numeropar);
+        }
+    }
+}
+    
+
