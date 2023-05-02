a6f7bb2c0651d3f6612442339194f63fada407f2
User <User@YORMAN>
2023-05-02 15:11:33 -0500

    Los numeros del 1 al 100

---
+++ Documents/NetBeansProjects/5ejercicios/src/ec/edu/institutosuperior/aplicacion/ejercicio3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.institutosuperior.aplicacion;
+
+/**
+ *
+ * @author User
+ */
+public class ejercicio3 {
+    public static void main(String[] args) {
+        int suma = 0;
+for (int i = 1; i <= 100; i++) {
+    suma += i;
+}
+System.out.println("La suma de los números del 1 al 100 es: " + suma);
+
+    }
+}
+
+
