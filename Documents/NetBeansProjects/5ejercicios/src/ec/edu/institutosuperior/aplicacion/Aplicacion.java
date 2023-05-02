2db4a479b04688a17f06db2dbb6ef70320d02701
User <User@YORMAN>
2023-05-01 20:54:42 -0500

    esta es la primera version v.1.0

--- Documents/NetBeansProjects/5ejercicios/src/ec/edu/institutosuperior/aplicacion/Aplicacion.java
+++ Documents/NetBeansProjects/5ejercicios/src/ec/edu/institutosuperior/aplicacion/Aplicacion.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ec.edu.institutosuperior.aplicacion;
 
-/**
- *
- * @author User
- */
+
 public class Aplicacion {
     public static void main(String[] args) {
         for(int x=0;x<=100;x++) {
