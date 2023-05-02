63cf173b332ca5c12245a14589a0e8f758d0c31f
User <User@YORMAN>
2023-05-02 15:14:19 -0500

    imprimir numeros pares del 2 al 20

---
+++ Documents/NetBeansProjects/5ejercicios/src/ec/edu/institutosuperior/aplicacion/ejercico5.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.institutosuperior.aplicacion;
+
+/**
+ *
+ * @author User
+ */
+public class ejercico5 {
+      public static void main(String[] args) {
+        for (int i = 2; i <= 20; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
