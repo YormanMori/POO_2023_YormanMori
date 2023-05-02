ba036f9d31a1ba83a7e29869b12c9bf6c8d30149
User <User@YORMAN>
2023-05-02 15:10:58 -0500

    Los numeros del 1 al 50

--- Documents/NetBeansProjects/5ejercicios/src/ec/edu/institutosuperior/aplicacion/Aplicacion.java
+++ Documents/NetBeansProjects/5ejercicios/src/ec/edu/institutosuperior/aplicacion/Aplicacion.java
@@ -4,7 +4,7 @@ package ec.edu.institutosuperior.aplicacion;
 
 public class Aplicacion {
     public static void main(String[] args) {
-        for(int x=0;x<=100;x++) {
+        for(int x=0;x<=50;x++) {
             System.out.println("El número es " + x);
         }
     }
